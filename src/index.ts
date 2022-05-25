68fbe2c057eaea1262c904bf47260a6508069201
Valeria Sanabria <103613127+ValeriaSanabria@users.noreply.github.com>
2022-05-25 16:44:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+let arregloMes: string [] = new Array(12)
+arregloMes[0] = "Enero";
+arregloMes[1] = "Febrero";
+arregloMes[2] = "Marzo";
+arregloMes[3] = "Abril";
+arregloMes[4] = "Mayo";
+arregloMes[5] = "Junio";
+arregloMes[6] = "Julio";
+arregloMes[7] = "Agosto";
+arregloMes[8] = "Septiembre";
+arregloMes[9] = "Octubre";
+arregloMes[10] = "Noviembre";
+arregloMes[12] = "Diciembre";
+
+let nroMes: number = 6
+let indice = nroMes - 1;
+console.log("El mes es " + arregloMes[indice]);
